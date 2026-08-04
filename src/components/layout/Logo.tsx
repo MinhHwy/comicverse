ad2c2865b2058a583ecaf5f7c87704b982a32705
MinhHwy <huyuwu55@gmail.com>
2026-08-04 11:29:24 +0700

    update ComicVerse logo

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { BookOpen } from "lucide-react";
+// import { BookOpen } from "lucide-react";
+import Image from "next/image";
 
 export default function Logo() {
   return (
@@ -7,11 +8,18 @@ export default function Logo() {
       href="/"
       className="flex items-center gap-2"
     >
-      <BookOpen className="h-8 w-8 text-orange-500" />
+      {/* <BookOpen className="h-8 w-8 text-orange-500" />
 
       <span className="text-3xl font-extrabold text-slate-800">
         ComicVerse
-      </span>
+      </span> */}
+      <Image
+  src="/logo-comicverse.png"
+  alt="ComicVerse"
+  width={180}
+  height={70}
+  priority
+/>
     </Link>
   );
 }
